02685be77a6c983fa10df2965abe3de27281a27b
jiaxiaoqing <2847341508@qq.com>
2019-01-21 08:18:29 +0800

    123

---
+++ MyMvcApplication/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+
+
+namespace MyMvcApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Photo photo = new Photo();
+            photo.Title = "12";
+            ViewBag.Title = "sdasdas";
+            return View(photo);
+        }
+    }
+}
---
+++ MyMvcApplication/Models/Photo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class Photo
+{
+    public virtual int PicID { get; set; }
+    public virtual int CharacterID { get; set; }
+    public virtual string Title { get; set; }
+    public virtual DateTime Time { get; set; }
+    public virtual string Character { get; set; }
+
+
+}
